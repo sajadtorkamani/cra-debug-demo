4bb1fcc10aa7f09171b4fc9f23949200c752c5ab
Sajad Torkamani <sajadtorkamani1@gmail.com>
2023-01-15 15:10:55 +0000

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from 'react'
+import './App.css'
 
 function App() {
+  useEffect(() => {
+    console.log('Hey')
+  }, [])
+
+  function printSomething() {
+    console.log('Hi')
+  }
+
+  function printSomethingElse() {
+    console.log('Hi 2')
+  }
+
   return (
     <div className="App">
       <p>Hello</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
